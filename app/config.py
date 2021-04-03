db88c7155f9a825e74d78bf962aff2dccdb8a0eb
vitaliy-ukiru <ender.111@yandex.ru>
2021-04-03 19:01:39 +0500

    add new field

--- app/config.py
+++ app/config.py
@@ -1,13 +1,21 @@
-import os
-from dotenv import load_dotenv
+from pathlib import Path
+
+from environs import Env
 
 
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+env = Env()
+env.read_env()
+
+
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMIN_ID = env.int("ADMIN_ID")
+ADMIN_USERNAME = env.str("ADMIN_USERNAME")
+PIN_ADMIN = f'@{ADMIN_USERNAME}'
+GITHUB_REPO = 'https://github.com/vitaliy-ukiru/fast-ss-bot'
+WORK_PATH: Path = Path(__file__).parent.parent
+
+
 position_buttons = ["Сверху", "Снизу"]
-
-
 BOT_COMMANDS = {
     'help': 'Справка по боту',
     'create': 'Создать сс',
