290f965ac96c070cb03f1e7c6a3cd1dca60a3c0b
vitaliy-ukiru <ender.111@yandex.ru>
2021-04-03 19:04:40 +0500

    Рефакторинг

--- app/loader.py
+++ app/loader.py
@@ -1,9 +1,16 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from app.config import BOT_TOKEN
 
 
-bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot, storage=MemoryStorage())
+storage = MemoryStorage()
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot=bot, storage=storage)
 
+
+__all__ = (
+    'bot',
+    'dp',
+    'storage'
+)
