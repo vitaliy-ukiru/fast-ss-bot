6c9759c3b4684f4a68f34fa58a3c314567cc7adc
Vitaliy <ender.111@yandex.ru>
2021-05-08 23:37:45 +0500

    add FSMMiddleware

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Dispatcher
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.contrib.middlewares.fsm import FSMMiddleware
 from loguru import logger
 
 
 def middlewares_setup(dp: Dispatcher):
     dp.middleware.setup(LoggingMiddleware())
+    dp.middleware.setup(FSMMiddleware())
     logger.info('Middlewares are successfully configured')
