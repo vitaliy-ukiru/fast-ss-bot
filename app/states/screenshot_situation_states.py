2f9361c33407818c564697ef751d642778e8e539
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2021-05-14 02:41:07 +0500

    rename state

--- app/states/screenshot_situation_states.py
+++ app/states/screenshot_situation_states.py
@@ -2,6 +2,6 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
 class ConstructorSG(StatesGroup):
-    get_background_image = State()
+    get_back_image = State()
     get_text_image = State()
     get_position = State()
